24aa152398b8ca70363617744c4655301e79eee2
nguyen duc huy <jumphigher777@gmail.com>
2021-05-28 18:19:48 +0700

    first init: tabbar, home, schedule, profile, login, register

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:clinicbookingapp/views/global/main-tab-bar.dart';
+import 'helpers/constants.dart';
+import 'views/home/home.dart';
+import 'dart:ui';
+
+final GlobalKey<NavigatorState> firstTabNavKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> secondTabNavKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> thirdTabNavKey = GlobalKey<NavigatorState>();
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Home',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+//        textTheme: Theme.of(context).textTheme.apply(bodyColor: Constants.BLACK),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: new CupertinoApp(
+        home: MainTabBar(),
+        localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+          DefaultMaterialLocalizations.delegate,
+          DefaultWidgetsLocalizations.delegate,
+        ],
+      ),
+    );
+  }
+
+}
+
+
